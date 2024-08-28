d0a3d8c57529090a16644a1400d7cd58ee789eb3
kimik6 <kakimia595@gmail.com>
2024-08-28 23:08:01 +0330

    make it ready for training

--- source/tools/train.py
+++ source/tools/train.py
@@ -20,6 +20,7 @@ from engines import train_fn
 parser = argparse.ArgumentParser()
 parser.add_argument("--dataset", type = str), parser.add_argument("--num_classes", type = int)
 parser.add_argument("--multilabel", action = "store_true")
+parser.add_argument("--pretrained", action = "store_true")
 parser.add_argument("--num_gpus", type = int, default = 1)
 args = parser.parse_args()
 config = {
@@ -52,6 +53,8 @@ train_loaders = {
 model = LightX3ECG(
     num_classes = 8, 
 )
+if args.pretrained :
+    model.load_state_dict(torch.load('/kaggle/working/LightX3ECG/source/best_ecg.pth'))
 if not config["is_multilabel"]:
     criterion = F.cross_entropy
 else:
